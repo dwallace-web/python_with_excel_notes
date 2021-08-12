92f4daf174a701fb6f10ffe6a7667955088484ff
David Wallace <dwallacemarketing@gmail.com>
2021-08-12 17:08:32 -0500

    readme updates

--- viewing_data.py
+++ viewing_data.py
@@ -1,25 +1,33 @@
-### Basic import 
 import pandas as pd
 from openpyxl.workbook import workbook
 
-- read file -  Importing data from each of these data sources is provided by function with the prefix read_*
+df = pd.read_csv('Names.csv', header=None)
 
-- header=None - be able to modify columns
-- delimiter='\' - work off tab spacing
-- .columns = ['this', 'is', 'a' ] - list of headers for columns that are defined
+# access data
+df.columns = ['first', 'last', 'address',
+              'city', 'State', 'Area Code', 'unknown']
 
-### Manipulating the DataFrame
-```
 # get data by columns
 print(df.columns)
 
 # show data from one column
 print(df['last'])
 
+# show data from first three rows
+print(df['city'][0:3])
+
+# print column / row only
+print(df.iloc[2])
+
+# show full data frame
+print(df)
+
+# pick specific value by index - state CO
+print(df.iloc[5, 4])
+
 # assign details to variable - get 3 columns from the data frame
 my_values = df[['first', 'last', 'Area Code']]
-
+# print??
+print(my_values)
 # save variable to excel document - index=None ---> ???
 saved_values = my_values.to_excel('name_area-cod.xlsx', index=None)
-
-```
